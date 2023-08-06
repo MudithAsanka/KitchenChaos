720637cbe0852fdd7087aeac04e164c441a2dd40
MudithAsanka <mudithasanka.dev@gmail.com>
2023-08-06 18:01:50 +0530

    ft[NewInputSystem] - Introduce new input system and refactor code

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions =  new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        // inputVector need to be normalized because when pressing two keys it's vector has large magnitude, that need to normalize 
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
