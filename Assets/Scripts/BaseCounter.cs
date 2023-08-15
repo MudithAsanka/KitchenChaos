03fa75a2e28dc396ee2b44e834475913d0b713e3
MudithAsanka <mudithasanka.dev@gmail.com>
2023-08-16 00:21:03 +0530

    ft[BaseCounter] - Create BaseCounter for any counter type, create ContainerCounter
    Create BaseCounter and refactor code for any counter type (Inheritance) and create ContainerCounter and visual animations

---
+++ Assets/Scripts/BaseCounter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent
+{
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
+    public virtual void Interact(Player player)
+    {
+        // virtual keyword use to implement this function in child classes in their own way
+        // Also can use abstract keyword and change it as abstract function
+        // Protected accessible can be used so child classes can access. But here this need to access by player, So make it as public
+        // This should never run
+        Debug.LogError("BaseCounter.Interact()");
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
