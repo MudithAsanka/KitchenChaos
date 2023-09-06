792f488aeea486bdde61f71191a809d1dcaede61
MudithAsanka <mudithasanka.dev@gmail.com>
2023-09-06 23:13:22 +0530

    ft[TrashCounter] - Create Trash Counter

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            // If Player holding something destroy it
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
