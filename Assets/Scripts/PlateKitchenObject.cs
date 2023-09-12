8ad7553e07b5d05dde981ddf8d017d32e463c97c
MudithAsanka <mudithasanka.dev@gmail.com>
2023-09-12 23:03:03 +0530

    ft[PlateVisuals] - Add plate holding ingredients, kitchenobjects visuals

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
     
     private List<KitchenObjectSO> KitchenObjectSOList;
@@ -29,6 +36,11 @@ public class PlateKitchenObject : KitchenObject
         {
             // Donot have this type
             KitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
     }
