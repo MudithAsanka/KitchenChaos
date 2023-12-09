1c8507cd7b9f0fd740a0a3a2387ad35dc521debc
MudithAsanka <mudithasanka.dev@gmail.com>
2023-12-09 13:11:37 +0530

    ft[OptionsUI] - Create OptionsUI, Manage volume of sound effects and music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance { get; private set; }
+
+    [SerializeField] private AudioSource audioSource;
+
+    private float volume = 0.3f;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        audioSource = GetComponent<AudioSource>();    
+    }
+
+    public void ChangeVolume()
+    {
+        volume += 0.1f;
+        if (volume > 1f)
+        {
+            // Back to volume 0
+            volume = 0f;
+        }
+        audioSource.volume = volume;
+    }
+
+    public float GetVolume()
+    {
+        return volume;
+    }
+}
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -10,6 +10,8 @@ public class SoundManager : MonoBehaviour
 
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
+
     private void Awake()
     {
         Instance = this;
@@ -65,13 +67,28 @@ public class SoundManager : MonoBehaviour
         PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    public void ChangeVolume()
+    {
+        volume += 0.1f;
+        if(volume > 1f)
+        {
+            // Back to volume 0
+            volume = 0f;
+        }
+    }
+
+    public float GetVolume()
+    {
+        return volume;
     }
 }
