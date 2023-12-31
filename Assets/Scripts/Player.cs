2dae0980d94ede194c0360bf2ae001495ee8fa50
MudithAsanka <mudithasanka.dev@gmail.com>
2023-12-31 19:20:25 +0530

    ft[PlayerCollision] - Prevent go through players by adding collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,7 +27,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private LayerMask countersLayerMask;
+    [SerializeField] private LayerMask collisionsLayerMask;
     [SerializeField] private Transform kitchenObjectHoldPoint;
+    [SerializeField] private List<Vector3> spawnPositionList;
 
     private bool isWalking;
     private Vector3 lastInteractDir;
@@ -54,6 +56,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
             // If this is local player
             LocalInstance = this;
         }
+
+        transform.position = spawnPositionList[(int)OwnerClientId]; // Spawn Players with different location offsets
+
         OnAnyPlayerSpawned?.Invoke(this, EventArgs.Empty);
     }
 
@@ -142,8 +147,8 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = 0.7f;
-        float playerHeight = 1f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        //float playerHeight = 1f; // Deprecated - For CapsulCast this was used
+        bool canMove = !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDir, Quaternion.identity, moveDistance, collisionsLayerMask);
 
         // Attempting moving diaganally scenario
         if (!canMove)
@@ -154,7 +159,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized; // normalized because diaganal speed need to normalized
             // moveDir.x != 0 -> Able to rotate when near the counter
             // For diaganal movement on gamepads need to consider here. for keyboards moveDir.x != 0 is fine
-            canMove = (moveDir.x < -0.5f || moveDir.x > 0.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = (moveDir.x < -0.5f || moveDir.x > 0.5f) && !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDirX, Quaternion.identity, moveDistance, collisionsLayerMask);
 
             if (canMove)
             {
@@ -169,7 +174,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized; // normalized because diaganal speed need to normalized
                 // moveDir.z != 0 -> Able to rotate when near the counter
                 // For diaganal movement on gamepads need to consider here. for keyboards moveDir.z != 0 is fine
-                canMove = (moveDir.z < -0.5f || moveDir.z > 0.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = (moveDir.z < -0.5f || moveDir.z > 0.5f) && !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDirZ, Quaternion.identity, moveDistance, collisionsLayerMask);
 
                 if (canMove)
                 {
