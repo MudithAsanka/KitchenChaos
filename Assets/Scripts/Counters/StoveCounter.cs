dbbcaf04a47c1beb87dc0ed306da384d1e547176
MudithAsanka <mudithasanka.dev@gmail.com>
2023-12-31 17:31:54 +0530

    ft[PlateKitchenObject] - Sync ingredients on plate

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -167,9 +167,9 @@ public class StoveCounter : BaseCounter, IHasProgress
                     // Player is holding a plate
                     if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
                     {
-                        GetKitchenObject().DestroySelf();
+                        KitchenObject.DestroyKitchenObject(GetKitchenObject());
 
-                        state.Value = State.Idle; // Otherwise next placing kitchenobject would be not idle state
+                        SetStateIdleServerRpc(); // Otherwise next placing kitchenobject would be not idle state
 
                     }
 
